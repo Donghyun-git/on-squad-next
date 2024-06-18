dfe810c9ba05c5ffbd8fe740d652375c38894e1b
an donghyun <ax34554@gmail.com>
2024-06-19 00:20:31 +0900

    chore: react-query prefetch 예제

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { squadListOptions } from "@/services/options/squadListOptions";
+import { getQueryClient } from "@/services/get-query-client";
 
 import { Button } from "@/components/ui/button";
 
-const Login = () => {
-  return <Button>로그인</Button>;
+const Login = async () => {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery(squadListOptions);
+
+  const info = await queryClient.getQueryData<Promise<{ title: string }[]>>([
+    "@test",
+  ]);
+
+  console.log(info, "test");
+
+  return (
+    <>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        {info?.map((item) => (
+          <>
+            <h1>{item.title}</h1>
+            <br />
+          </>
+        ))}
+        <Button>로그인</Button>
+      </HydrationBoundary>
+    </>
+  );
 };
 
 export default Login;

6004000e6437c57ec7ded2330bfdb5cffac89ecd
an donghyun <ax34554@gmail.com>
2024-07-22 01:07:52 +0900

    chore: 세팅, assets

--- next.config.js
+++ next.config.js
@@ -2,12 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    autoLabel: 'always',
+  },
 
   async rewrites() {
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'http://192.168.0.11:8080/api/v1/:path*',
+        destination: 'http://192.168.1.142:8083/api/v1/:path*',
       },
     ];
   },

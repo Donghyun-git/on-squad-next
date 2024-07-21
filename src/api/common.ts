081554b1eab5e0e535b24d713105ba7b6f6d9909
an donghyun <ax34554@gmail.com>
2024-07-22 01:10:29 +0900

    feat: fetch request, response interceptor

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,33 +1,49 @@
-import createHttp from 'http-request-fetch';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../app/api/auth/[...nextauth]/route';
+import returnFetch, { FetchArgs } from 'return-fetch';
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const apiFetch = async <T>(
+  input: URL | RequestInfo,
+  init?: RequestInit,
+): Promise<{ data: T; status: number }> => {
+  return returnFetch({
+    baseUrl: 'http://192.168.1.142:8083',
+    headers: { Accept: 'application/json' },
+    interceptors: {
+      request: async (config: FetchArgs) => {
+        const session = await getServerSession(authOptions);
 
-const DEFAULT_CONFIG = { headers: { 'Content-Type': 'application/json' } };
+        if (session) {
+          if (!config[1]) {
+            config[1] = {};
+          }
+          if (!config[1].headers) {
+            config[1].headers = {};
+          }
 
-/**
- * @example
- *   apiFetch
-    .get("https://www.na-holo.site/api/recipes")
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
- */
-export const apiFetch = createHttp(BASE_URL, DEFAULT_CONFIG);
+          // Headers 객체를 변환하여 Authorization 헤더 추가
+          const headers = new Headers(config[1].headers);
 
-apiFetch.registerInterceptor({
-  onRequest: (config) => {
-    // request를 보내기 전 수행할 로직
-    return config;
-  },
-  onResponse: (response) => {
-    // response를 받고나서 수행할 로직
-    return response;
-  },
-  onRequestError: (reason) => {
-    // request 과정에서 에러가 발생할 경우 수행할 로직
-    return Promise.reject(reason);
-  },
-  onResponseError: (reason) => {
-    // response 과정에서 에러가 발생할 경우 수행할 로직
-    return Promise.reject(reason);
-  },
-});
+          headers.set('Authorization', `Bearer ${session.accessToken}`);
+
+          config[1].headers = Object.fromEntries(headers.entries());
+        }
+
+        return config;
+      },
+
+      response: async (response: Response) => {
+        const responseBody = await response.json();
+
+        return new Response(JSON.stringify(responseBody), {
+          status: response.status,
+        });
+      },
+    },
+  })(input, init).then((response) =>
+    response.json().then((data: T) => ({
+      data,
+      status: response.status,
+    })),
+  );
+};

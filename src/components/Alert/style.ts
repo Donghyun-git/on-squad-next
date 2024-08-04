996a2b14e2a0e1274118a3e4818521a8829d742a
donghyun-git <donghyun1113@hanmail.net>
2024-08-04 20:39:30 +0900

    feat: Alert 버튼 스타일 상수

---
+++ src/components/Alert/style.ts
@@ -0,0 +1,6 @@
+export const BUTTON = Object.freeze({
+  ACTION:
+    'text-white text-lg pt-6 pb-5 rounded-none rounded-br-md bg-primary500 hover:bg-primary600 active:bg-primary700',
+  CANCEL:
+    'text-grayscale600 text-lg pt-6 pb-5 rounded-none rounded-bl-md bg-grayscale100 hover:bg-grayscale200 active:bg-grayscale300',
+});
